5b43f9459623e79d575b1e9f893fa8ca78051013
Aryan Bisht <164388972+Aryanb10@users.noreply.github.com>
2026-02-17 23:42:43 +0530

    Time: 15 ms (70.03%), Space: 23.6 MB (95.36%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, int>map;
+        int num=0;
+        string s;
+        vector<vector<string>> ans;
+        if(strs.size()<2){
+            ans.push_back({strs[0]});
+            return ans;
+        }
+        for(int i=0;i<strs.size();i++){
+            s=strs[i];
+            sort(s.begin(), s.end());
+            if(map.find(s)==map.end()){
+                map.insert({s,num});
+                ans.push_back({strs[i]});
+                num++;
+            }
+            else{
+                ans[map[s]].push_back({strs[i]});
+            }
+        }
+        return ans;
+    }
+};
