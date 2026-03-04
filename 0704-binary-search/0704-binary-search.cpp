4faa1f4a45b81f4060ba7401ae41ac1cf1e19e87
Aryan Bisht <164388972+Aryanb10@users.noreply.github.com>
2026-03-05 00:14:43 +0530

    Time: 0 ms (100%), Space: 31.5 MB (7.74%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        unordered_set<int>s;
-        for(int i=0; i<nums.size(); i++){
-            s.insert(nums[i]);
-            if(s.find(target)!= s.end()){
-            return i;
+        int left=0, right=nums.size()-1;
+        while(left<right){
+            int mid=(left+right)>>1;
+            if(nums[mid]>=target){
+                right=mid;
+            }
+            else{
+                left=mid+1;
             }
         }
-        return -1;
+        return nums[left]==target ? left: -1;
     }
 };
