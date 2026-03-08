390a8f2ca5bfac04b00d882f751aa093e0d1c20a
Aryan Bisht <164388972+Aryanb10@users.noreply.github.com>
2026-03-08 22:57:04 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.69%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,7 +3,7 @@ public:
     int search(vector<int>& nums, int target) {
         int left=0, right=nums.size()-1;
         while(left<right){
-            int mid=(left+right)>>1;
+            int mid=(left+right)/2;
             if(nums[mid]>=target){
                 right=mid;
             }
