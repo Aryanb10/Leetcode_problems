718ae4a94b388215787833bc3e0b956a385344f4
Aryan Bisht <164388972+Aryanb10@users.noreply.github.com>
2026-05-30 02:34:49 +0530

    Time: 0 ms (100%), Space: 10 MB (97.24%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int prev_1, prev_2=0;
-        for(auto n: nums){
-            int dummy= max(prev_1+n, prev_2);
+        for(int n: nums){
+            int curr= max(prev_1+n, prev_2);
             prev_1=prev_2;
-            prev_2=dummy;
+            prev_2=curr;
         }
         return prev_2;
     }
